a17f95f24f9defcef114fdc11dcfa92bac304bb1
Jon Vickers <Jonathan.lane.vickers@gmail.com>
2023-07-11 12:34:43 -0500

    fixed issue with acm.tf file

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -8,10 +8,38 @@ resource "aws_acm_certificate" "ssl_certificate" {
   lifecycle {
     create_before_destroy = true
   }
+
+data "aws_route53_zone" "vickers.codes" {
+  name         = "vickers.codes"
+  private_zone = false
+}
 }
 
-resource "aws_acm_certificate_validation" "cert_validation" {
-  certificate_arn = aws_acm_certificate.ssl_certificate.arn
+resource "aws_route53_record" "vickers.codes" {
+  for_each = {
+    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.example.zone_id
 }
 
+resource "aws_acm_certificate_validation" "vickers.codes" {
+  certificate_arn         = aws_acm_certificate.example.arn
+  validation_record_fqdns = [for record in aws_route53_record.example : record.fqdn]
+}
+
+resource "aws_lb_listener" "vickers.codes" {
+  # ... other configuration ...
+
+  certificate_arn = aws_acm_certificate_validation.example.certificate_arn
+}
 
