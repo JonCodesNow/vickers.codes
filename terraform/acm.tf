059df365044487612d53cd509b1da9216e9d9264
Jon Vickers <Jonathan.lane.vickers@gmail.com>
2023-07-17 20:34:13 -0500

    terraform changes

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -3,17 +3,17 @@ resource "aws_acm_certificate" "ssl_certificate" {
   domain_name               = var.domain_name
   subject_alternative_names = ["*.${var.domain_name}"]
   #validation_method         = "EMAIL"
-  validation_method = "DNS"
+  validation_method         = "DNS"
+}
 
-  data "aws_route53_zone" "vickers_codes" {
-    name         = "vickers.codes"
-    private_zone = false
-  }
+data "aws_route53_zone" "example" {
+  name         = "vickers.codes"
+  private_zone = false
 }
 
 resource "aws_route53_record" "vickers_codes" {
   for_each = {
-    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.ssl_certificate.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
@@ -28,14 +28,12 @@ resource "aws_route53_record" "vickers_codes" {
   zone_id         = data.aws_route53_zone.example.zone_id
 }
 
-resource "aws_acm_certificate_validation" "vickers_codes" {
-  certificate_arn         = aws_acm_certificate.example.arn
-  validation_record_fqdns = [for record in aws_route53_record.example : record.fqdn]
+resource "aws_acm_certificate_validation" "example" {
+  certificate_arn         = aws_acm_certificate.ssl_certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.vickers_codes : record.fqdn]
 }
 
-resource "aws_lb_listener" "vickers_codes" {
-  # ... other configuration ...
 
-  certificate_arn = aws_acm_certificate_validation.example.certificate_arn
-}
+
+
 
--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -51,10 +51,8 @@ resource "aws_cloudfront_distribution" "www_s3_distribution" {
     }
   }
 
-  viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate_validation.cert_validation.certificate_arn
-    ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.1_2016"
+viewer_certificate {
+    cloudfront_default_certificate = true
   }
 }
 
